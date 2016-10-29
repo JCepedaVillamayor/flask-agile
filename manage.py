1164197a869b3f33688c77acc933842a7e47162b
jcepedavillamayor <jcepedavillamayor@gmail.com>
2016-10-29 16:42:25 +0200

    Flask Script added

--- manage.py
+++ manage.py
@@ -1,8 +1,9 @@
 from app import create_app
+from flask_script import Manager, Shell
+
 
 app = create_app()
-
-
+manager = Manager(app)
 
 if __name__ == "__main__":
-    app.run()
+    manager.run()
